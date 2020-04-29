c8a812257a4e80867883bbd7e1e586dae4864ffb
Avinash Kumar <44208272+avinashmallik62@users.noreply.github.com>
2020-04-29 23:28:59 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.uielementsdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    public void showText(View view){
+        textView.setVisibility(View.VISIBLE);
+    }
+    public void hideText(View view){
+        textView.setVisibility(View.INVISIBLE);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+    }
+}
